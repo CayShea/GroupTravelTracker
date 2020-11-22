3d786741264aeff74b2f588170cb9623e047962e
CayShea <cayleesheaharrington@gmail.com>
2020-11-22 16:55:56 -0700

    email confirmation modal

--- frontend/react_app/src/pages/SignUp.js
+++ frontend/react_app/src/pages/SignUp.js
@@ -3,11 +3,8 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,12 +13,15 @@ import { connect } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import * as actions from '../store/authActions';
+import ConfirmEmailModel from '../components/modals/ConfirmEmailModal';
 
 
 function SignUp(props) {
   const classes = useStyles();
   const [email, setEmail] = React.useState(null);
   const [password, setPassword] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
   let history = useHistory();
   let location = useLocation();
   let { from } = location.state || { from: { pathname: "/" } }
@@ -36,12 +36,18 @@ function SignUp(props) {
       case 'password': setPassword(event.target.value); break;
       default: return null;
     }
- }
+ };
 
  const handleSubmit = (e) => {
   e.preventDefault();
   props.onAuth(email, password);
- }
+  setOpen(true);
+ };
+
+ const handleClose = () => {
+  setOpen(false);
+  window.location.reload();
+ };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -117,6 +123,9 @@ function SignUp(props) {
             </Grid>
           </Grid>
         </form>
+        <div>
+          <ConfirmEmailModel open={open} handleClose={handleClose}/>
+        </div>
       </div>
     </Container>
   );
