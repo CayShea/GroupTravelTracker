63f855cc31b8d43bc4eee07903239e1be1ddc4b6
CayShea <cayleesheaharrington@gmail.com>
2021-03-02 15:30:39 -0700

    trial for templates override

--- backend/grouptraveltracker_api/grouptraveltracker_api/settings.py
+++ backend/grouptraveltracker_api/grouptraveltracker_api/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'grouptraveltracker_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
